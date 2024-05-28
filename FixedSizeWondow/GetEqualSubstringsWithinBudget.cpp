0d47f1f78e7dabb66a21ef798e421000330992ca
Himanshu Bhange <himanshubhange886@gmail.com>
2024-05-28 19:38:55 +0530

    add Get Equal Substrings WIthinBudget

--- FixedSizeWondow/GetEqualSubstringsWithinBudget.cpp
+++ FixedSizeWondow/GetEqualSubstringsWithinBudget.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <math.h>
 using namespace std;
 
 /*
@@ -11,7 +12,7 @@ Q) -> You are given two strings s and t of the same length and an integer maxCos
 
     Return the maximum length of a substring of s that can be changed to be the same as the corresponding substring of t with a cost less than or equal to maxCost. If there is no substring from s that can be changed to its corresponding substring from t, return 0.
 
- 
+
 
 Example 1:
 
@@ -21,15 +22,39 @@ Explanation: "abc" of s can change to "bcd".
 That costs 3, so the maximum length is 3.
 */
 
-class Solution {
+class Solution
+{
 public:
-    int equalSubstring(string s, string t, int maxCost) {
-        
+    int equalSubstring(string s, string t, int maxCost)
+    {
+        int i = 0;
+        int j = 0;
+        int ans = INT_MIN;
+        int windowSize = 0;
+        int n = s.size();
+        int totalCOst = 0;
+
+        if (maxCost == 0)
+            return 1;
+
+        while (j < n)
+        {
+            totalCOst += abs(s[j] - t[j]);
+
+            while(totalCOst > maxCost) {
+                totalCOst -= abs(s[i] - t[i]);
+                i++;
+            }
+
+            windowSize = j - i + 1;
+            ans = max(ans, windowSize);
+            j++;
+        }
+        return ans;
     }
 };
 int main()
 {
-    
 
     return 0;
 }
